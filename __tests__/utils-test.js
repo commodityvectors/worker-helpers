b917260342b2c29845f8944bd811815221d014d3
Rafael Grillo <grillorafael@gmail.com>
2016-02-14 13:57:44 +0000

    add getProperty method

--- __tests__/utils-test.js
+++ __tests__/utils-test.js
@@ -3,18 +3,49 @@
 
     jest.dontMock('../modules/utils');
 
-    describe('clone', () => {
-        beforeEach(() => {
-            require('../modules/utils');
-        })
+    beforeEach(() => {
+        require('../modules/utils');
+    });
 
+    describe('clone', () => {
         it('clones an object', function() {
-            var object = {a: 1, b: 2};
-            var clone = utils.copy(object);
+            let object = {a: 1, b: 2};
+            let clone = utils.copy(object);
             clone.a = 2;
 
             expect(clone).not.toBe(object);
             expect(object.a).not.toBe(2);
+        });
+    });
+
+    describe('getProperty', () => {
+        let object;
+
+        beforeEach(() => {
+            object = {
+                a: {
+                    b: 2,
+                    c: {
+                        d: 4
+                    }
+                },
+                e: 10
+            };
+        });
+
+        it('fetchs an object property by given path', () => {
+            expect(utils.getProperty(object, 'a')).toBe(object.a);
+            expect(utils.getProperty(object, 'a.b')).toBe(2);
+            expect(utils.getProperty(object, 'a.c.d')).toBe(4);
+        });
+
+        it('returns undefined if invalid property', () => {
+            expect(utils.getProperty(object, 'd.e.f.g')).toBe(undefined);
+        });
+
+        it('works on arrays as well', () => {
+            object = [{a: 1}]
+            expect(utils.getProperty(object, '0.a')).toBe(1);
         });
     });
 })();
--- modules/utils.js
+++ modules/utils.js
@@ -5,6 +5,13 @@
         copy: (o) => {
             // Rude but still much faster than most recursive clone methods
             return JSON.parse(JSON.stringify(o));
+        },
+        getProperty: (o, p) => {
+            const propList = p.split('.');
+            const firstProperty = propList.shift();
+            return propList.reduce((currentValue, property) => {
+                return currentValue ? currentValue[property] : currentValue;
+            }, o[firstProperty]);
         }
     }
 
