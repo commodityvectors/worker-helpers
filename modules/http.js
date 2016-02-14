81e825930759ce8f88fb4147dc6f25902b04ac5a
Rafael Grillo <grillorafael@gmail.com>
2016-02-14 13:40:20 +0000

    Refactor and fix method issues

--- modules/http.js
+++ modules/http.js
@@ -21,51 +21,52 @@
         }
     }
 
-    var http = {
-        run(opts) {
-            return new Promise((resolve, reject) => {
-                var xmlHttp = new XMLHttpRequest();
-                xmlHttp.onreadystatechange = () => {
-                    if (xmlHttp.readyState === 4) {
-                        if(xmlHttp.status === 200) {
-                            resolve(JSON.parse(xmlHttp.responseText));
-                        }
-                        else {
-                            reject({
-                                status: xmlHttp.status,
-                                data: xmlHttp.responseText
-                            });
-                        }
+    function run(opts) {
+        return new Promise((resolve, reject) => {
+            var xmlHttp = new XMLHttpRequest();
+            xmlHttp.onreadystatechange = () => {
+                if (xmlHttp.readyState === 4) {
+                    if(xmlHttp.status >= 200 && xmlHttp.status <= 299) {
+                        resolve(JSON.parse(xmlHttp.responseText));
                     }
-                };
-                xmlHttp.open(opts.method, opts.url, true);
-                if(opts.method === 'POST') {
-                    xmlhttp.setRequestHeader('Content-Type', 'application/json;charset=UTF-8');
+                    else {
+                        reject({
+                            status: xmlHttp.status,
+                            data: xmlHttp.responseText
+                        });
+                    }
                 }
-                xmlHttp.send(opts.data ? JSON.stringify(opts.data) : '');
-            });
-        },
-        get(url, params) {
+            };
+            xmlHttp.open(opts.method, opts.url, true);
+            if(opts.method === 'POST') {
+                xmlHttp.setRequestHeader('Content-Type', 'application/json;charset=UTF-8');
+            }
+            xmlHttp.send(opts.data ? JSON.stringify(opts.data) : '');
+        });
+    }
+
+    var http = {
+        run: run,
+        get: (url, params) => {
             var parsedParams = parseURLParams(url, params);
             url = parsedParams.newUrl;
             params = encodeParams(parsedParams.params);
-            return this.run({
+            return run({
                 method: 'GET',
                 url: `${url}?${params}`,
             });
         },
-        post(url, params) {
+        post: (url, params) => {
             var parsedParams = parseURLParams(url, params);
             url = parsedParams.newUrl;
-            params = encodeParams(parsedParams.params);
-            return this.run({
-                method: 'GET',
+            return run({
+                method: 'POST',
                 url: url,
-                data: params
+                data: parsedParams.params
             });
         }
     };
 
     c.http = http;
     return http;
-})(exports || self || window);
+})(self || window);
